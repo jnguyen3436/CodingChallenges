c641c444e1901e79b5eec4ef63c248fa733afdf2
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-25 10:10:04 -0500

    added some challenges

---
+++ codeWars/MexicanWave.js
@@ -0,0 +1,15 @@
+function wave (text) {
+  
+    let finalArray = []
+  for ( let i = 0; i < text.length; i++) {
+      let arr = text.split('')
+      //console.log(arr)
+      if ( arr[i] === ' ') { 
+         continue
+      }
+      arr[i] = arr[i].toUpperCase()  
+      //console.log(arr)
+      finalArray.push(arr.join(''))
+      }
+      return finalArray
+  }
