233b222b1469890e62a4717c6b11d2ec9f77b4e0
jnguyen3436 <jnguyen3436@gmail.com>
2019-12-26 23:16:44 -0500

    coding challenge

---
+++ codeWars/5kyu/bananafish.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class bananafish extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
