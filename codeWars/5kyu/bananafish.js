708d00113a381b212cfc54d1df9b3013e756c821
jnguyen3436 <jnguyen3436@gmail.com>
2019-12-28 21:00:31 -0500

    more challenges done

--- codeWars/5kyu/bananafish.js
+++ codeWars/5kyu/bananafish.js
@@ -4,7 +4,7 @@ export default class bananafish extends Component {
     render() {
         return (
             <div>
-                
+                asdfasdfas
             </div>
         )
     }
